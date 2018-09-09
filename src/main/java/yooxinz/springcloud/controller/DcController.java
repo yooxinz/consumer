723a288eba8f2087cca38729023132d80832bade
yooxinz <zhangyongxing89@gmail.com>
2018-09-09 21:14:45 +0800

    [ADD] init ribbon consumer

--- src/main/java/yooxinz/springcloud/controller/DcController.java
+++ src/main/java/yooxinz/springcloud/controller/DcController.java
@@ -1,8 +1,6 @@
 package yooxinz.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,15 +12,11 @@ import org.springframework.web.client.RestTemplate;
 public class DcController {
 
     @Autowired
-    LoadBalancerClient loadBalancerClient;
-    @Autowired
     RestTemplate restTemplate;
 
     @GetMapping("/consumer")
     public String dc() {
-        ServiceInstance serviceInstance = loadBalancerClient.choose("local");
-        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/hi";
-        System.out.println(url);
-        return restTemplate.getForObject(url, String.class);
+        return restTemplate.getForObject("http://local/hi", String.class);
+
     }
 }
