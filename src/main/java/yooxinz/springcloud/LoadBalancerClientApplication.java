6a323ab77b26c70ca0e2250aa9a18eba53b11db5
yooxinz <zhangyongxing89@gmail.com>
2018-09-09 21:09:48 +0800

    [ADD] init load balance clinet

---
+++ src/main/java/yooxinz/springcloud/LoadBalancerClientApplication.java
@@ -0,0 +1,21 @@
+package yooxinz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class LoadBalancerClientApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBalancerClientApplication.class, args);
+	}
+}
---
+++ src/main/java/yooxinz/springcloud/controller/DcController.java
@@ -0,0 +1,28 @@
+package yooxinz.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by star on 2018/9/9.
+ */
+@RestController
+public class DcController {
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("local");
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/hi";
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+}
